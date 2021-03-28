512889052091b3aba24d00d8774d8193903285ec
神浦晃行 <akiyuki@Akiyuki-no-MBP.local>
2021-03-28 22:43:45 +0900

    場所追加 Backdrop の文字が超過しないように

--- lib/widgets/places/addition_form/PlaceAdditionPreviewArea.dart
+++ lib/widgets/places/addition_form/PlaceAdditionPreviewArea.dart
@@ -4,7 +4,6 @@ import 'package:togolist/models/PlaceItem.dart';
 import 'package:togolist/widgets/places/detail/PlaceDetailImageArea.dart';
 
 class PlaceAdditionPreviewArea extends StatelessWidget {
-
   PlaceItem placeItem = null;
 
   PlaceAdditionPreviewArea({this.placeItem});
@@ -17,17 +16,23 @@ class PlaceAdditionPreviewArea extends StatelessWidget {
         height: 200,
         width: double.infinity,
         color: Colors.black12.withOpacity(0.05),
-        child: Icon(Icons.image_not_supported_outlined, color: Colors.black38, size: 70),
+        child: Icon(Icons.image_not_supported_outlined,
+            color: Colors.black38, size: 70),
       );
     }
   }
 
   Widget buildPlacePreviewTitleArea() {
     Icon icon = (this.placeItem != null)
-        ? Icon(Icons.add_location_alt_outlined, color: ColorSettings.primaryColor)
+        ? Icon(Icons.add_location_alt_outlined,
+            color: ColorSettings.primaryColor)
         : Icon(Icons.add_location_alt_outlined, color: Colors.black54);
     Widget name = (this.placeItem != null && this.placeItem.name != null)
-        ? Text(this.placeItem.name, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold), overflow: TextOverflow.ellipsis,)
+        ? Text(
+            this.placeItem.name,
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            overflow: TextOverflow.ellipsis,
+          )
         : Text("場所が選択されていません", style: TextStyle(fontSize: 16));
 
     return Container(
@@ -39,10 +44,9 @@ class PlaceAdditionPreviewArea extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
               icon,
-              Padding(
-                  padding: EdgeInsets.only(left: 10),
-                  child: name
-              )
+              Expanded(
+                  child:
+                      Padding(padding: EdgeInsets.only(left: 10), child: name))
             ],
           ),
           if (this.placeItem != null)
