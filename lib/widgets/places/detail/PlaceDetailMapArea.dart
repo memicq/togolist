cad189d75552b5336615e48e35dec4479bf0e8ce
神浦晃行 <akiyuki@Akiyuki-no-MBP.local>
2021-04-08 21:55:36 +0900

    近傍マップ

--- lib/widgets/places/detail/PlaceDetailMapArea.dart
+++ lib/widgets/places/detail/PlaceDetailMapArea.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:togolist/models/MapMarker.dart';
 import 'package:togolist/widgets/places/detail/PlaceDetailItemCard.dart';
 
@@ -14,7 +15,27 @@ class PlaceDetailMapArea extends StatelessWidget {
             children: [
               PlaceDetailItemCard(
                   title: "近傍マップ",
-                  content: ColoredBox(color: Colors.red)
+                  content: Container(
+                    width: double.infinity,
+                    height: 200,
+                    child: GoogleMap(
+                      rotateGesturesEnabled: false,
+                      scrollGesturesEnabled: false,
+                      zoomGesturesEnabled: false,
+                      myLocationButtonEnabled: false,
+                      myLocationEnabled: true,
+                      markers: [
+                        Marker(
+                          markerId: MarkerId(marker.googlePlaceId),
+                          position: LatLng(marker.latitude, marker.longitude),
+                        )
+                      ].toSet(),
+                      initialCameraPosition: CameraPosition(
+                        target: LatLng(marker.latitude, marker.longitude),
+                        zoom: 15
+                      ),
+                    )
+                  )
               ),
               PlaceDetailItemCard(
                   title: "最寄り駅",
